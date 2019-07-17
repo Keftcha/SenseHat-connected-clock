64d5a09afebb32c04c186d3e4d861602eb504c59
Keftcha <alban.dewilde@gmail.com>
2019-07-18 00:24:20 +0200

    menu_config is now independant to the main clock
    we can change the foreground color

--- menu_config.py
+++ menu_config.py
@@ -1,7 +1,39 @@
-#!/usr/env python3
+#!/usr/bin/env python3
 
-def change_fg_color(colors, curent_color):
-    None
+from sense_hat import SenseHat
+import json
+
+from utilities import calculate_brightness, display_pixels
+
+def change_fg_color(
+    colors_name, colors, brightness,
+    value, fg_color, bg_color, instance
+):
+    # Find the idx of our color
+    clr = colors_name.index(fg_color)
+
+    # Change the color
+    new_color = colors_name[(clr + value) % len(colors_name)]
+
+    # Get the brightness of the new color
+    new_color_code = calculate_brightness(brightness, colors[new_color])
+    bg_code = calculate_brightness(brightness, colors[bg_color])
+
+    pixels_state = [
+        1, 1, 1, 1, 0, 1, 1, 1,
+        1, 0, 0, 0, 1, 0, 0, 0,
+        1, 0, 0, 0, 1, 0, 0, 0,
+        1, 1, 1, 0, 1, 0, 0, 0,
+        1, 0, 0, 0, 1, 0, 1, 1,
+        1, 0, 0, 0, 1, 0, 0, 1,
+        1, 0, 0, 0, 1, 0, 0, 1,
+        1, 0, 0, 0, 0, 1, 1, 0
+    ]
+
+    display_pixels(pixels_state, instance, new_color_code, bg_code)
+
+    return new_color
+
 
 
 def change_bg_color(colors):
@@ -27,15 +59,22 @@ def menu_config(
     brightness_multiplicator,
     rotate
 ):
-    # Config function and arguments they need
+    # List of color name
+    colors_name = list(possibles_colors.keys())
+
+    # Config function and specific arguments they need
     functions = {
-        change_fg_color: (possibles_colors, fg_color)
+        change_fg_color: (
+            colors_name,
+            possibles_colors,
+            brightness_multiplicator
+        )
         #change_bg_color: possibles_colors,
         #change_brightness: brightness_multiplicator,
         #change_rotate: rotate
     }
 
-    # Value of parameters (must be in the same order of `functions`
+    # Value of configured parameters must be in the same order of `functions`
     values = [
         fg_color,
         bg_color,
@@ -45,19 +84,49 @@ def menu_config(
 
     idx = 0
 
-    for event in sense.stick.get_events():
-        if event.action == "pressed":
-            if event.direction == "middle":
-                return (
-                    fg_color,
-                    bg_color,
-                    rotate,
-                    brightness_multiplicator
-                )
-            if event.direction == "right":
-                idx += 1
-            if event.direction == "left":
-                idx -= 1
+    while True:
 
-    idx = idx % len(functions)
-    values[idx] = list(functions.keys())[idx](*list(functions.values())[idx])
+        # The value is the changer parameter,
+        # if he is positive, the function will encreate the changed parameter
+        # if he us negative, the function will decrease the changed parameter
+        # otherwise, if it's 0 nothing will happend
+        value = 0
+
+        for event in sense.stick.get_events():
+            if event.action == "pressed":
+                if event.direction == "middle":
+                    return values
+                if event.direction == "right":
+                    idx += 1
+                if event.direction == "left":
+                    idx -= 1
+
+                if event.direction == "up":
+                    value = 1
+                if event.direction == "down":
+                    value = -1
+
+        idx = idx % len(functions)
+        values[idx] = list(functions.keys())[idx](
+            *list(functions.values())[idx],
+            value,
+            values[0],    # fg_colog
+            values[1],    # bg_color
+            sense
+        )
+
+if __name__ in "__main__":
+    sense = SenseHat()
+    sense.set_rotation(180)
+
+    with open("colors.json", "r") as colors:
+        possibles_colors = json.load(colors)
+
+    print(menu_config(
+        sense,
+        possibles_colors,
+        "blue",
+        "yellow",
+        1,
+        180
+    ))
