85aa0be4f9f890f7a3209e90f8dd7267e850f1b9
Keftcha <alban.dewilde@gmail.com>
2019-07-28 20:53:31 +0200

    Make a diaporama functionality

---
+++ diaporama.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from sense_hat import SenseHat
+from time import sleep
+import os
+
+
+def diaporama(instance, directory_path, time_sleep=3):
+    """Display 8*8 pictures in the directory `directory_path`
+    and wait `time_sleep` seconde between each"""
+    pictures = os.listdir(directory_path)
+
+    idx = 0
+    while True:
+        sense.clear()
+        instance.load_image(directory_path + pictures[idx])
+        sleep(time_sleep)
+        idx += 1
+        idx = idx % len(pictures)
+
+        # Break if the joystick is toutched
+        if sense.stick.get_events():
+            break
+
+
+if __name__ in "__main__":
+    sense = SenseHat()
+    sense.set_rotation(180)
+
+    diaporama(sense, "./pictures/", 1.5)
